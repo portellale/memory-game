8c5ee21543985156da262ea3407749f0425466b1
portellale <portella_le@hotmail.com>
2024-04-29 15:50:08 -0300

    finish project

--- js/login.js
+++ js/login.js
@@ -1,22 +1,23 @@
-const input = document.querySelector('.login__input');
-const button = document.querySelector('.login__button');
-const form = document.querySelector('.login-form');
+const input = document.querySelector(".login__input");
+const button = document.querySelector(".login__button");
+const form = document.querySelector(".login__form");
 
+//Valida o input pegando o target do evento
 const validateInput = ({ target }) => {
-  if (target.value.length > 3) {
-    button.removeAttribute('disabled');
+  if (target.value.length > 2) {
+    button.removeAttribute("disabled");
     return;
   }
+  button.setAttribute("disabled", "");
+};
 
-  button.setAttribute('disabled', '');
-}
-
+//Salva o que foi digitado no input
 const handleSubmit = (event) => {
   event.preventDefault();
 
-  localStorage.setItem('player', input.value);
-  window.location = 'pages/game.html';
-}
+  localStorage.setItem("player", input.value);
+  window.location = "pages/game.html";
+};
 
-input.addEventListener('input', validateInput);
-form.addEventListener('submit', handleSubmit);
+input.addEventListener("input", validateInput);
+form.addEventListener("submit", handleSubmit);
